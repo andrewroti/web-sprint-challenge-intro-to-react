8e7bb7e3f4c6df3e945817c7961339a28600467d
andrewroti <andrewroti@gmail.com>
2021-02-06 20:50:19 -0500

    working on character component and calling the swapi api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Character from './components/Character.js';
+import axios from 'axios';
+
+// const count = 0;
+const apiURL = 'https://swapi.dev/api/people/1/'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,11 +13,40 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [name, setName] = useState();
+  const [gender, setGender] = useState();
+  
+
+  useEffect(()=>{
+
+    
+    axios.get(apiURL)
+    .then(res =>{
+      
+      setName(res.data.name);
+      setGender(res.data.gender);
+    })
+    .catch(err=>{
+      console.log(err.data);
+    })
+
+  }, [])
+  console.log(name)
+  
+  
+
+  // const getChar = data =>{
+  //   const count = 0;
+  //   const apiURL = `https://swapi.dev/api/people/${count}/`
+
+  //   let addChar = char.map()
+  // }
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-      <h2>Test</h2>
+            
+      <Character charName={name} gender={gender}/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
 // Write your Character component here
+import React from 'react';
+
+
+
+const Character = (props)=>{
+
+    const {charName, gender} = props;
+
+    return(
+    <div>
+        <h3>My name is: {charName}</h3>
+        <div>And I am a: {gender}</div>
+    </div>
+    );
+} 
+
+export default Character;
