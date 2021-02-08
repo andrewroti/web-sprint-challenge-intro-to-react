348a13803802207111fb16fa0d006649d8ca14c4
andrewroti <andrewroti@gmail.com>
2021-02-08 01:13:24 -0500

    gave up on importing data.  just used handler.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Character from './components/Character.js';
 import axios from 'axios';
-import {data} from './mocks/handlers.js'
+import {data} from './mocks/handlers.js';
 
 // const count = 0;
-const apiURL = 'https://swapi.dev/api/people/'
-
+const apiURL = 'https://swapi.dev/api/people/1/'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -15,32 +14,35 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-  const [char, setChar] = useState(data);
-  // const [gender, setGender] = useState();
-  // const [hairColor, setHairColor] = useState();
-  // const [eyeColor, setEyeColor] = useState();
-  // const [birthYear, setBirthYear] = useState();
-  console.log(char);
+  const [char, setName] = useState(data);
+  const [gender, setGender] = useState();
+  const [hairColor, setHairColor] = useState();
+  const [eyeColor, setEyeColor] = useState();
+  const [birthYear, setBirthYear] = useState();
   
 
   // useEffect(()=>{
-  //   axios.get(apiURL)
-  //   .then(res=>{
-  //     // setChar(res.data);
-  //     // const charData = Array.from(res.data);
-  //     // console.log(charData[0].name);
-  //     setChar(res.data);
-  //     console.log(char);
-      
 
+    
+  //   axios.get(apiURL)
+    
+    
+  //   .then(res =>{
+      
+  //     setName(res.data.name);
+  //     setGender(res.data.gender);
+  //     setBirthYear(res.data.birth_year);
+  //     setEyeColor(res.data.eye_color);
+  //     setHairColor(res.data.hair_color);
+      
   //   })
   //   .catch(err=>{
   //     console.log(err.data);
   //   })
-  // }, [char])
 
+  // }, [])
   
- 
+  
   
   
 
@@ -54,8 +56,8 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
-            
-      {char.map((chars)=>(<Character chars={chars}/>))}
+          
+      {char.map((charData)=> (<Character char={charData}/>))}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -5,18 +5,18 @@ import React from 'react';
 
 const Character = (props)=>{
 
-    const {chars} = props;
+    const {char} = props;
 
     return(
     <div>
-        <h3>Click My Name To Learn More: {chars.name}</h3>
+        <h3>{char.name}</h3>
         <div className='info_container'>
             <ul>
-                <li>Name: {chars.name}</li>
-                <li>Gender: {chars.gender}</li>
-                <li>Birth Year: {chars.birth_year}</li>
-                <li>Eye Color: {chars.eye_color}</li>
-                <li>Hair Color: {chars.hair_color}</li>
+                <li>Name: {char.name}</li>
+                <li>Gender: {char.gender}</li>
+                <li>Birth Year: {char.birth_year}</li>
+                <li>Eye Color: {char.eye_color}</li>
+                <li>Hair Color:{char.hair_color}</li>
             </ul>
         </div>
     </div>
